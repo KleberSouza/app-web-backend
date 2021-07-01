ddf7a0f2d9c3bbaa9abc923d7fe5c9d7dbba2752
KleberSouza <kleberjfsouza@gmail.com>
2021-07-01 11:54:30 -0300

    feat: Adicionar o model Veiculo.

---
+++ app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório Informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o placa!")]
+        public string Placa { get; set; }
+    }
+}
